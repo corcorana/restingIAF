2d20d4eea6d735c6e1a1d7bf7fcc60c8952519a0
acor0004 <andrew.corcoran1@monash.edu>
2018-08-09 07:21:46 +1000

    remove nchans input arg, upper bound to cmin input, contingency against channel NaNs, expand help

--- restingIAF.m
+++ restingIAF.m
@@ -1,4 +1,4 @@
-function [pSum, pChans, f] = restingIAF(data, nchan, cmin, fRange, Fs, w, Fw, k, varargin)
+function [pSum, pChans, f] = restingIAF(data, cmin, fRange, Fs, w, Fw, k, varargin)
 % Primary function for running `restingIAF` analysis routine for estimating
 % two indices of individual alpha frequency (IAF): Peak alpha frequency 
 % (PAF) and the alpha mean frequency / centre of gravity (CoG).
@@ -11,51 +11,61 @@ function [pSum, pChans, f] = restingIAF(data, nchan, cmin, fRange, Fs, w, Fw, k,
 % channels by `chanMeans`.
 %
 % This function and all custom-designed dependencies are part of the 
-% `restingIAF` package, (c) Andrew W. Corcoran, 2016-2017.
+% `restingIAF` package, (c) Andrew W. Corcoran, 2016--2018.
 %
-% Visit github.com/corcorana/restingIAF for licence, updates, and further
-% information about package development and testing.
+% Please consult our methods paper for information about recommended 
+% parameter settings and performance on empirical and simulated EEG
+% signals (please cite this paper if our code was useful for your work): 
+%
+% Corcoran, A.W., Alday, P.M., Schlesewsky, M., & Bornkessel-Schlesewsky, 
+%   I. (2018). Toward a reliable, automated method of individual alpha 
+%   frequency (IAF) quantification. Psychophysiology, 55(7), e13064. 
+%   doi: 10.1111/psyp.13064. 
+%
+% You may also cite the code version used in your analysis so that others 
+% may be able to fully replicate your results (see README.md).
+%
+% Visit github.com/corcorana/restingIAF for further info on licencing and
+% updates on package development.
 %
 %% Outputs:
-%   pSum = structure containing summary statistics
-%   pChans = structure containing channel-wise spectral and alpha parameter data
-%   f = trimmed vector of frequency bins resolved by `pwelch`
+%   pSum    = structure containing summary statistics of alpha-band parameters
+%   pChans  = structure containing channel-wise spectral and alpha parameter data
+%   f       = trimmed vector of frequency bins resolved by `pwelch`
 %
 %% Required inputs:
-%   data = vector or matrix containing continuous EEG channel data
-%   nchan = number of channels in data
-%   cmin = minimum number of channel estimtes that must be resolved in
-%          order to calculate average PAF/CoG estimates
-%   fRange = frequency range to be included in analysis (e.g., 1-40 Hz )
-%   Fs = EEG sampling rate
-%   w = bounds of alpha peak search window (e.g., [7 13])
-%   Fw = frame width, Savitzky-Golay filter (corresponds to number of freq. 
-%          bins spanned by filter, must be odd)
-%   k = polynomial order, Savitzky-Golay filter (must be < Fw)
+%   data    = vector or matrix containing continuous EEG channel data
+%             (matrix rows = channels, cols = sample points)
+%   cmin    = minimum number of channel estimtes that must be resolved in
+%             order to calculate average PAF/CoG estimates
+%   fRange  = frequency range to be included in analysis (e.g., [1, 40] Hz)
+%   Fs      = EEG sampling rate
+%   w       = bounds of alpha peak search window (e.g., [7 13])
+%   Fw      = frame width, Savitzky-Golay filter (corresponds to number of  
+%             freq. bins spanned by filter; must be odd)
+%   k       = polynomial order, Savitzky-Golay filter (must be < Fw)
 %
 %% Optional inputs:
 %
-%   mpow = error bound (standard deviation) used to determine threshold differentiating 
-%          substantive peaks from background spectral noise (default = 1)
-%   mdiff = minimal height difference distinguishing a primary peak from
-%           competing peaks (default = 0.20)
-%   taper = taper window function applied by `pwelch` (default = 'hamming')
-%   tlen = length of taper window applied by `pwelch` (default = 4*Fs)
-%   tover = length of taper window overlap in samples (default = 50% window length)
-%   nfft = specify number of FFT points used to calculate PSD (default = next power of 2 above window length)
-%   norm = normalise power spectra (default = true)
-
+%   mpow    = error bound (s.d.) used to determine threshold differentiating 
+%             substantive peaks from background spectral noise (default = 1)
+%   mdiff   = minimal height difference distinguishing a primary peak from
+%             competing peaks (default = 0.20; i.e. 20% peak height)
+%   taper   = taper window function applied by `pwelch` (default = 'hamming')
+%   tlen    = length of taper window applied by `pwelch` (default = 4 sec)
+%   tover   = length of taper window overlap in samples (default = 50% window length)
+%   nfft    = specify number of FFT points used to calculate PSD (default = 
+%             next power of 2 above window length)
+%   norm    = normalise power spectra (default = true)
+%
 %% setup inputParser
 p = inputParser;
 p.addRequired('data',...
                 @(x) validateattributes(x, {'numeric'}, ...
                 {'2d', 'nonempty'}));
-p.addRequired('nchan',...
-                @(x) validateattributes(x, {'numeric'}, ...
-                {'scalar', 'integer', 'positive'}));
 p.addRequired('cmin',...
                 @(x) validateattributes(x, {'numeric'}, ...
-                {'scalar', 'integer', 'positive'}));
+                {'scalar', 'integer', 'positive', '<=', size(data, 1)}));
 p.addRequired('fRange',...
                 @(x) validateattributes(x, {'numeric'}, ...
                 {'integer', 'nonnegative', 'increasing', 'size', [1,2]}));
@@ -71,7 +81,7 @@ p.addRequired('Fw',...
 p.addRequired('k',...
                 @(x) validateattributes(x, {'numeric'}, ...
                 {'scalar', 'integer', 'positive', '<', Fw }));
-
+   
 p.addOptional('mpow', 1,...
                 @(x) validateattributes(x, {'numeric'}, ...
                 {'scalar', 'positive'}));
@@ -94,53 +104,67 @@ p.addOptional('norm', true,...
                 @(x) validateattributes(x, {'logical'}, ...
                 {'scalar'}));
             
-p.parse(data, nchan, cmin, fRange, Fs, w, Fw, k, varargin{:})
+p.parse(data, cmin, fRange, Fs, w, Fw, k, varargin{:})
 
-mpow = p.Results.mpow;
-mdiff = p.Results.mdiff;
-taper = p.Results.taper;
-tlen = p.Results.tlen;
-tover = p.Results.tover;
-nfft = p.Results.nfft;
-norm = p.Results.norm;
+mpow    = p.Results.mpow;
+mdiff   = p.Results.mdiff;
+taper   = p.Results.taper;
+tlen    = p.Results.tlen;
+tover   = p.Results.tover;
+nfft    = p.Results.nfft;
+norm    = p.Results.norm;
 %%
+% struct for channel data (PSD estimates & derivatives, some additional info)
+pChans = struct('pxx', [], 'minPow', [], 'd0', [], 'd1', [], 'd2', [],...
+    'peaks', [], 'pos1', [], 'pos2', [], 'f1', [], 'f2', [], 'inf1', [], 'inf2', [],...
+    'Q', [],'Qf', [], 'gravs', [], 'selP', [], 'selG', [] );      
 
-pChans = struct('pxx', [], 'minPow', [], 'd0', [], 'd1', [], 'd2', [], 'peaks', [], 'pos1', [], 'pos2', [], 'f1', [], 'f2', [], 'inf1', [], 'inf2', [], 'Q', [],'Qf', [], 'gravs', [], 'selP', [], 'selG', [] );      % struct for channel data (PSD estimates & derivatives, some additional info)
+nchan = size(data, 1);      % number of channels in data matrix
 
 for kx = 1:nchan
-	% perform pwelch routine to extract PSD estimates by channel
-    fh = str2func(taper);
-    [pxx, f] = pwelch(data(kx,:), fh(tlen), tover, nfft, Fs);
+    if sum(isnan(data(kx,:)))==0      % ensure no channel NaNs
 
-  	% delimit range of freq bins to be included in analysis
- 	frex = dsearchn(f, fRange(1)):dsearchn(f, fRange(2));      
-   	f = f(frex);
-	pxx = pxx(frex);      % truncate PSD to frex range
+        % perform pwelch routine to extract PSD estimates by channel
+        fh = str2func(taper);
+        [pxx, f] = pwelch(data(kx,:), fh(tlen), tover, nfft, Fs);
 
-   	% normalise truncated PSD
-   	if norm == true
-        pChans(kx).pxx = pxx / mean(pxx);            
+        % delimit range of freq bins to be included in analysis
+        frex = dsearchn(f, fRange(1)):dsearchn(f, fRange(2));      
+        f = f(frex);
+        pxx = pxx(frex);      % truncate PSD to frex range
+
+        % normalise truncated PSD
+        if norm == true
+            pChans(kx).pxx = pxx / mean(pxx);            
+        else
+            pChans(kx).pxx = pxx;
+        end
+
+        % calculate minPower vector
+        [pfit, sig] = polyfit(f, log10(pChans(kx).pxx), 1);     % fit 1st order poly (regression line) to normalised spectra (log-scaled)
+        [yval, del] = polyval(pfit, f, sig);                    % derive yval coefficients of fitted polynomial and delta (std dev) error estimate
+        pChans(kx).minPow = yval + (mpow * del);                % takes [minPowThresh * Std dev] as upper error bound on background spectral noise
+
+        % apply Savitzky-Golay filter to fit curves to spectra & estimate 1st and 2nd derivatives
+        [pChans(kx).d0, pChans(kx).d1, pChans(kx).d2] = sgfDiff(pChans(kx).pxx, Fw, k, Fs, tlen);
+
+        % calculate frequency resolution
+        exp_tlen = nextpow2(tlen);
+        fres = Fs/2.^exp_tlen;
+
+        % take derivatives, find peak(s) and boundaries of alpha band
+        [pChans(kx).peaks, pChans(kx).pos1, pChans(kx).pos2, pChans(kx).f1, pChans(kx).f2,...
+            pChans(kx).inf1, pChans(kx).inf2, pChans(kx).Q, pChans(kx).Qf]...
+            = peakBounds(pChans(kx).d0, pChans(kx).d1, pChans(kx).d2, f, w,...
+            pChans(kx).minPow, mdiff, fres);
+    
     else
-        pChans(kx).pxx = pxx;
+        warning('Row #%s contains NaNs, skipping channel...', num2str(kx))
+        nchan = nchan-1;    % trim nchans for cx loop later on
     end
     
-  	% calculate minPower vector
-  	[pfit, sig] = polyfit(f, log10(pChans(kx).pxx), 1);     % fit 1st order poly (regression line) to normalised spectra (log-scaled)
-    [yval, del] = polyval(pfit, f, sig);                    % derive yval coefficients of fitted polynomial and delta (std dev) error estimate
-  	pChans(kx).minPow = yval + (mpow * del);                % takes [minPowThresh * Std dev] as upper error bound on background spectral noise
-                
-  	% apply Savitzky-Golay filter to fit curves to spectra & estimate 1st and 2nd derivatives
-    [pChans(kx).d0, pChans(kx).d1, pChans(kx).d2] = sgfDiff(pChans(kx).pxx, Fw, k, Fs, tlen);
-
-  	% calculate frequency resolution
-  	exp_tlen = nextpow2(tlen);
-  	fres = Fs/2.^exp_tlen;
-        
-   	% take derivatives, find peak(s) and boundaries of alpha band
-   	[pChans(kx).peaks, pChans(kx).pos1, pChans(kx).pos2, pChans(kx).f1, pChans(kx).f2, pChans(kx).inf1, pChans(kx).inf2, pChans(kx).Q, pChans(kx).Qf] = peakBounds(pChans(kx).d0, pChans(kx).d1, pChans(kx).d2, f, w, pChans(kx).minPow, mdiff, fres);
-
 end
-        
+
 % estimate gravities for smoothed spectra (average IAF window across channels)
 [ gravs, selG, iaw ] = chanGravs([pChans(:).d0], f, [pChans(:).f1], [pChans(:).f2] );
 
@@ -148,15 +172,17 @@ end
 [ selP, pSum ] = chanMeans(gravs, selG, [pChans(:).peaks], [pChans(:).d0], [pChans(:).Qf], cmin); 
 
 % retain gravity estimates and selected channels in channel data struct
-for ix = 1:nchan
-    pChans(ix).gravs = gravs(ix);
-    pChans(ix).selP = selP(ix);
-    pChans(ix).selG = selG(ix); 
+% (only loop through trimmed channels)
+for cx = 1:nchan
+    pChans(cx).gravs = gravs(cx);
+    pChans(cx).selP = selP(cx);
+    pChans(cx).selG = selG(cx); 
 end
     
 % get total number of chans that contributed to PAF/CoG estimation
 pSum.pSel = sum(selP);
 pSum.gSel = sum(selG);
 pSum.iaw = iaw;
+
 
 end
